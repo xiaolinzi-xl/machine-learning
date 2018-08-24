f570f1cd57869feadf263cff31ad3ddb95f3a85c
xiaolinzi-xl <daoshangxl@163.com>
2018-08-24 17:02:54 +0800

    简单的线性回归实现

---
+++ 05. 线性回归法/03-simple-linear-regression/playML/SimpleLinearRegression.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+class SimpleLinearRegression1:
+
+    def __init__(self):
+        '''初始化Simple Linear Regression模型'''
+        self.a_ = None
+        self.b_ = None
+
+    def fit(self, x_train, y_train):
+        '''根据训练数据集训练模型'''
+        assert x_train.ndim == 1, ''
+        assert len(x_train) == len(y_train), ''
+
+        x_mean = np.mean(x_train)
+        y_mean = np.mean(y_train)
+
+        num = 0.0
+        d = 0.0
+
+        for x_i, y_i in zip(x_train, y_train):
+            num += (x_i - x_mean) * (y_i - y_mean)
+            d += (x_i - x_mean) ** 2
+
+        self.a_ = num / d
+        self.b_ = y_mean - self.a_ * x_mean
+
+        return self
+
+    def predict(self, x_predict):
+        '''给定待预测数据集，返回预测结果'''
+        assert x_predict.ndim == 1, ''
+        assert self.a_ is not None and self.b_ is not None, ''
+
+        return np.array([self._predict(x) for x in x_predict])
+
+    def _predict(self, x_single):
+        '''给定单个预测数据，返回预测结果值'''
+        return self.a_ * x_single + self.b_
+
+    def __repr__(self):
+        return 'SimpleLinearRegression1()'
