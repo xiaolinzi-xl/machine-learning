1eb9a3388b6d90dacc93f36132575a03af9c27b7
xiaolinzi-xl <daoshangxl@163.com>
2018-08-28 15:12:30 +0800

    衡量线性回归算法的指标：R Square

---
+++ 05. 线性回归法/06-R-squared/playML/SimpleLinearRegression.py
@@ -0,0 +1,45 @@
+import numpy as np
+from .metrics import r2_score
+
+
+class SimpleLinearRegression:
+
+    def __init__(self):
+        '''初始化Simple Linear Regression模型'''
+        self.a_ = None
+        self.b_ = None
+
+    def fit(self, x_train, y_train):
+        '''根据训练数据集训练模型'''
+        assert x_train.ndim == 1, ''
+        assert len(x_train) == len(y_train), ''
+
+        x_mean = np.mean(x_train)
+        y_mean = np.mean(y_train)
+
+        num = (x_train - x_mean).dot(y_train - y_mean)
+        d = (x_train - x_mean).dot(x_train - x_mean)
+
+        self.a_ = num / d
+        self.b_ = y_mean - self.a_ * x_mean
+
+        return self
+
+    def predict(self, x_predict):
+        '''给定待预测数据集，返回预测结果'''
+        assert x_predict.ndim == 1, ''
+        assert self.a_ is not None and self.b_ is not None, ''
+
+        return np.array([self._predict(x) for x in x_predict])
+
+    def _predict(self, x_single):
+        '''给定单个预测数据，返回预测结果值'''
+        return self.a_ * x_single + self.b_
+
+    def score(self, x_test, y_test):
+        '''确定当前模型的准确度'''
+        y_predict = self.predict(x_test)
+        return r2_score(y_test, y_predict)
+
+    def __repr__(self):
+        return 'SimpleLinearRegression()'
